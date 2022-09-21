5448bd0ff284a9747af4e1e832cb7f673df96f6c
Daniel Villamizar <daniel8106@hotmail.com>
2022-09-21 09:43:09 -0500

    Update configureSQL.ps1

--- configureSQL.ps1
+++ configureSQL.ps1
@@ -9,11 +9,11 @@
 $response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.usgovcloudapi.net' -UseBasicParsing -Method GET -Headers @{Metadata="true"}
 $content = $response.Content | ConvertFrom-Json
 $KeyVaultToken = $content.access_token
-$akv_Content = (Invoke-WebRequest -Uri "https://$keyVaultName.vault.usgovcloudapi.net/secrets/AzDevOpsSqlPass?api-version=2016-10-01" -UseBasicParsing -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).content
+$akv_Content = (Invoke-WebRequest -Uri "https://$keyVaultName.vault.azure.net/secrets/AzDevOpsSqlPass?api-version=2016-10-01" -UseBasicParsing -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}).content
 $value = ($akv_Content | ConvertFrom-JSON).value
 $Password = $value
 
-$targetsrvfull = $targetsrv + ".database.usgovcloudapi.net"
+$targetsrvfull = $targetsrv + ".database.windows.net"
 
 #Create query strings to update databases
 $masterUpdate = "CREATE USER " + $managedIdentity + " FROM EXTERNAL PROVIDER;ALTER ROLE [dbmanager] ADD MEMBER " + $managedIdentity +";"
